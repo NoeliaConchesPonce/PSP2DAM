0cdb6ff06716a5c336a8777dacdb4b47df97b430
NoeliaConchesPonce <57366330+NoeliaConchesPonce@users.noreply.github.com>
2021-10-28 15:22:04 +0200

    EjercicioEvaluable2 sin comentar

---
+++ AE_T2_Multiproceso/Multiprocesos/src/es/florida/AEV2/Sumador.java
@@ -0,0 +1,48 @@
+package es.florida.AEV2;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+
+public class Sumador {
+
+	public static double sumar(String nombreNEO, double posicionNEO, double velocidadNEO){
+		double posicionTierra = 1;
+		double velocidadTierra = 100;
+		for (int i = 0; i < (50 * 365 * 24 * 60 * 60); i++) {
+		posicionNEO = posicionNEO + velocidadNEO * i;
+		posicionTierra = posicionTierra + velocidadTierra * i;
+		}
+		double resultado = 100 * Math.random() *
+			Math.pow( ((posicionNEO-posicionTierra)/(posicionNEO+posicionTierra)), 2);
+		
+		return resultado;
+		}
+		
+		public static void main(String[] args){
+		Sumador s = new Sumador();
+		String nombreNEO = args[0]; 
+		double posicionNEO = Double.parseDouble(args[1]);
+		double velocidadNEO = Double.parseDouble(args[2]);
+		double resultado = s.sumar(nombreNEO, posicionNEO, velocidadNEO);
+		
+		
+		if(resultado > 10) {
+			System.err.println("ALERTA MUNDIAL El asteroide " + nombreNEO + "tiene un porcentaje de colision de " + String.format("%.2f", resultado) + "%" + "\n");
+		}else {
+			System.out.println("TRANQUILIDAD  El asteroide " + nombreNEO + "tiene un porcentaje de colision de " + String.format("%.2f", resultado) + "%" + "\n");
+		}
+		
+		//mostrar la probabilidad de colision 
+		try {
+            File fichero = new File(nombreNEO + ".txt");
+            FileWriter fw = new FileWriter(fichero);
+            BufferedWriter bw = new BufferedWriter(fw);
+            bw.write("La probilidad de colision es de: " + String.format("%.2f", resultado) + "%");
+            bw.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+		}
+
+}
